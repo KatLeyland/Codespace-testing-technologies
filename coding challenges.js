556c55c3dc0473a42209adbb46133006a394b43d
KatLeyland <112875876+KatLeyland@users.noreply.github.com>
2022-11-02 11:17:48 +0000

    Added Jorge version

--- coding challenges.js
+++ coding challenges.js
@@ -7,7 +7,7 @@ console.log(reverseString("John"));
 console.log(reverseString("James"));
 
 //Coding task 2 - reverse array
-
+//my solution
 function reverseArray(array){
     var output = [];
     for (var i = array.length-1; i >= 0; i--){
@@ -18,6 +18,10 @@ return output;
 
 console.log(reverseArray([1, 2, 3, 4, 5]));
 console.log(reverseArray(["I", "like", "JavaScript"]));
+//jorge solution
+
+const newReverseArray = (array) => array.reverse();
+console.log(newReverseArray([1, 2, 3, 4, 5, 6, 7, 8, 9]))
 
 //Coding task 3 - output most expensive item calculated by price*stock
 
